9bd11d583cf6d22bd6ba414cabe181fafc6294a9
Carlos Espinal <cespinalb@gmail.com>
2023-08-18 14:03:27 -0600

    2 pm commit

---
+++ Numero.cpp
@@ -0,0 +1,19 @@
+#include "Numero.h"
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+Numero::Numero() { cdigitos = NULL; }
+
+Numero::Numero(char digits[2]) {
+	cdigitos = digits;
+}
+
+vector<Numero*> Numero::procesarCadena() {
+	vector<Numero*> vnumero;
+	for (size_t i = 0; i < 2; i++){
+
+	}
+	
+
+}
