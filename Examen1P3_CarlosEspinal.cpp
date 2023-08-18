39105d3a3b79bca67e3bf2bdadffd50d6fc15cbc
Carlos Espinal <cespinalb@gmail.com>
2023-08-18 16:56:48 -0600

    final final

--- Examen1P3_CarlosEspinal.cpp
+++ Examen1P3_CarlosEspinal.cpp
@@ -3,7 +3,7 @@
 #include "Numero.h";
 #include "Rectangulo.h"
 using namespace std;
-
+//menu
 void menu() {
     cout << "---- MENU PRINCIPAL ----\n" << "Opcion 1: \n" << "Opcion 2: \n" << "Opcion 3: SALIDA \n\n" << "Ingrese la opcion:  ";
 }
@@ -23,11 +23,12 @@ int main(){
                     cout << "Ingrese una cadena de dos numeros: ";
                     cin.ignore();
                     getline(cin, sdigitos);
-             
+             //llamado de la clase
                 n = Numero(sdigitos);
                 n.general();
                     break;
             case 2:
+                //llamado de la clase
                 Rectangulo();
 
                     break;
