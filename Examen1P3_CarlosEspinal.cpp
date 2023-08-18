922f77d05e5453a2220972fa5653f9b30451d693
Carlos Espinal <cespinalb@gmail.com>
2023-08-18 13:34:22 -0600

    Primer commit

---
+++ Examen1P3_CarlosEspinal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+void menu() {
+    cout << "---- MENU PRINCIPAL ----\n" << "Opcion 1: \n" << "Opcion 2: \n" << "Opcion 3: SALIDA \n";
+}
+
+int main(){
+    int opcion;
+    menu();
+    
+}
---
+++ Numero.h
@@ -0,0 +1,9 @@
+#pragma once
+class Numero{
+	int numero;
+	char letras[26] = { 'a','b', 'c' ,'d', 'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
+
+public:
+
+};
+
